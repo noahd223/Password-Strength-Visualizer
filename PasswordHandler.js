b307a2ca5a33ffdc9cf4326c3542632df8f53a27
Noah Der Garabedian <noahd3485@gmail.com>
2023-09-19 17:01:44 -0400

    Built basic html part of the site and created the PasswordHandler class.

---
+++ PasswordHandler.js
@@ -0,0 +1,14 @@
+class PasswordHandler {
+    // constructs the PasswordHandler using the entered password in the text field
+    constructor() {
+        this.password = document.getElementById("Passwordtextbox");
+    }
+
+    calcLength() {
+        
+    }
+    
+
+
+
+}
