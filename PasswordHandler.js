3bfc61adb9c8788cf2b518022b28083bd7093449
Noah Der Garabedian <noahd3485@gmail.com>
2023-09-21 23:14:05 -0400

    Re-organized index.html and added instance methods to the PasswordHandler to write an algorithm to assign a password a score based on its strength.

--- PasswordHandler.js
+++ PasswordHandler.js
@@ -1,12 +1,32 @@
+
 class PasswordHandler {
     // constructs the PasswordHandler using the entered password in the text field
-    constructor() {
-        this.password = document.getElementById("Passwordtextbox");
+    constructor(password) {
+        this.password = password;
     }
+    constructor(password) {
+        this.password = password;
+    }
+    // gets score based on the password's length using a logorithmic function
+    getLengthScore() {
+        const passwordLength = this.password.length;
+        const lengthScore = ((20*(passwordLength**2))-(5*passwordLength)-50)/(passwordLength**2)
+        return lengthScore;
+    }
+    // Creates a score based on the symbols in the password, uses symbolScoresDictionary to
+    // determine each characters score
+    getSymbolScore() {
+        let symbolScore = 0;
+        for (let i = 0; i < this.password.length; i++) {
+            const currentCharacter = this.password.charAt(i);
+            if (currentCharacter in symbolScoresDictionary) {
+                symbolScore += symbolScoresDictionary.currentCharacter;
+            }
+        }
+        return symbolScore;
+    }
+     
 
-    calcLength() {
-        
-    }
     
 
 
