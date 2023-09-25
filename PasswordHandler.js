03c2b6813122eed709b391cb10cc1747837b4547
Noah Der Garabedian <noahd3485@gmail.com>
2023-09-24 22:54:16 -0400

    Need to fix NaN errors

--- PasswordHandler.js
+++ PasswordHandler.js
@@ -4,17 +4,16 @@ class PasswordHandler {
     constructor(password) {
         this.password = password;
     }
-    constructor(password) {
-        this.password = password;
-    }
+   
     // gets score based on the password's length using a logorithmic function
+    // see the function graphed here: https://www.desmos.com/calculator/2kh2t3r9yb
     getLengthScore() {
         const passwordLength = this.password.length;
-        const lengthScore = ((20*(passwordLength**2))-(5*passwordLength)-50)/(passwordLength**2)
+        const lengthScore = 15 - (15 / (1 + Math.pow(passwordLength / 9, 2.3)));
         return lengthScore;
     }
     // Creates a score based on the symbols in the password, uses symbolScoresDictionary to
-    // determine each characters score
+    // determine each character's score
     getSymbolScore() {
         let symbolScore = 0;
         for (let i = 0; i < this.password.length; i++) {
@@ -25,10 +24,33 @@ class PasswordHandler {
         }
         return symbolScore;
     }
-     
-
-    
-
+    /* Checks if the password contains a common word
+     * returns true if the password contains consecutive characters that form a common word
+     * Uses CommonPasswords.json as a list of common words
+     * ex: password returns true while pass3wo4rd returns false
+    */
+    isCommonWord() {
+        const commonPasswords = fs.readFileSync("./CommonPasswords.txt").toString('utf-8'); // converts the text file into an array
+        for (const commonpassword of commonPasswords) {
+            if (this.password.includes(commonpassword)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /* Computes the current password score by adding the length and symbol scores
+     * if the password has a common word in it or no symbols, the method caps the score at 3
+     */
+    computePasswordScore() {
+        let passwordScore = this.password.getLengthScore + this.password.getSymbolScore;
+        if (this.password.isCommonWord || this.password.getSymbolScore === 0) {
+            if (passwordScore > 3) {
+                return 3;
+            }
+            return passwordScore;
+        }
+        return this.password.getLengthScore;
+    }
 
 
 }
